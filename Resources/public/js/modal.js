6388e1cf4b424f8efd1a77abfe9c3fac24a4844f
Adam Misiorny <adam.misiorny@gmail.com>
2014-07-01 13:53:42 +0200

    move click event from image to parent div in media select modal

--- Resources/public/js/modal.js
+++ Resources/public/js/modal.js
@@ -1,252 +1,252 @@
 (function ($) {
 
-	if(typeof window.btnMedia === 'object') {
-		return false;
-	}
+    if(typeof window.btnMedia === 'object') {
+        return false;
+    }
 
-	$(document).ready(function(){
-		var modalEl			= null;
-		var modalElWrap		= null;
-		var button 			= '<div class="btn btn-primary">Select media</div>';
-		var deleteButton    = '<div class="btn btn-danger" style="margin:0 0 0 5px;">Delete</div>';
-		var openedFrom 		= null;
-		var paginationUrl 	= '';
-		var modalUrl 		= $('script[data-remote-url]').attr('data-remote-url');
-		var mediaSelects 	= $('select.btn-media');
+    $(document).ready(function(){
+        var modalEl         = null;
+        var modalElWrap     = null;
+        var button          = '<div class="btn btn-primary">Select media</div>';
+        var deleteButton    = '<div class="btn btn-danger" style="margin:0 0 0 5px;">Delete</div>';
+        var openedFrom      = null;
+        var paginationUrl   = '';
+        var modalUrl        = $('script[data-remote-url]').attr('data-remote-url');
+        var mediaSelects    = $('select.btn-media');
 
-		if(typeof modalUrl === 'undefined') {
-			console.log('No modal url specified');
-			return;
-		}
+        if(typeof modalUrl === 'undefined') {
+            console.log('No modal url specified');
+            return;
+        }
 
-		var updateButton = function(el, select) {
-			$(el).text($(select).find('option:selected').text());
-		};
+        var updateButton = function(el, select) {
+            $(el).text($(select).find('option:selected').text());
+        };
 
-		var resetButton = function(el) {
-			$(el).text('Choose image');
-		};
+        var resetButton = function(el) {
+            $(el).text('Choose image');
+        };
 
-		var getPaginationSearchPart = function(el) {
-			var page 		= $(el).find('a').attr('href').split('?')[1];
+        var getPaginationSearchPart = function(el) {
+            var page        = $(el).find('a').attr('href').split('?')[1];
 
-			return page;
-		};
+            return page;
+        };
 
-		var updateModalBody = function(url) {
-			$.get(url, function(response){
-				modalEl.find('.modal-body').fadeOut(function(){
-					$(this).html(response).fadeIn(function(){
-						bindModalBehaviors();
-					});
-				});
-			});
-		};
+        var updateModalBody = function(url) {
+            $.get(url, function(response){
+                modalEl.find('.modal-body').fadeOut(function(){
+                    $(this).html(response).fadeIn(function(){
+                        bindModalBehaviors();
+                    });
+                });
+            });
+        };
 
-		var bindPagination = function() {
-			modalEl.find('.modal-body .pagination').on('click', 'li', function(e) {
-				if(!$(this).hasClass('disabled') && !$(this).hasClass('active')) {
-					var urlSearchPart 	= getPaginationSearchPart(this);
-					var url 			= paginationUrl + '?' + urlSearchPart;
+        var bindPagination = function() {
+            modalEl.find('.modal-body .pagination').on('click', 'li', function(e) {
+                if(!$(this).hasClass('disabled') && !$(this).hasClass('active')) {
+                    var urlSearchPart   = getPaginationSearchPart(this);
+                    var url             = paginationUrl + '?' + urlSearchPart;
 
-					updateModalBody(url);
-				}
+                    updateModalBody(url);
+                }
 
-				return false;
-			});
-		};
+                return false;
+            });
+        };
 
-		var bindCategoryFilter = function() {
-			modalEl.find('.category-filter').change(function(){
-				var val = $(this).val();
+        var bindCategoryFilter = function() {
+            modalEl.find('.category-filter').change(function(){
+                var val = $(this).val();
 
-				if(val != 0) {
-					var url = paginationUrl + '?category=' + val;
-					updateModalBody(url);
-				}
-				else {
-					updateModalBody(paginationUrl);
-				}
-			});
-		};
+                if(val != 0) {
+                    var url = paginationUrl + '?category=' + val;
+                    updateModalBody(url);
+                }
+                else {
+                    updateModalBody(paginationUrl);
+                }
+            });
+        };
 
-		var bindModalNavigation = function() {
-			bindPagination();
-			bindCategoryFilter();
-		};
+        var bindModalNavigation = function() {
+            bindPagination();
+            bindCategoryFilter();
+        };
 
-		var callback = function(id) {
-			var select 	= $(openedFrom).prev('select.btn-media');
+        var callback = function(id) {
+            var select  = $(openedFrom).prev('select.btn-media');
 
-			select.val(id);
-			updateButton(openedFrom, select);
-		};
+            select.val(id);
+            updateButton(openedFrom, select);
+        };
 
-		var bindModalBehaviors = function() {
-			modalEl.find('.modal-body style, .modal-body link').appendTo($('head'));
+        var bindModalBehaviors = function() {
+            modalEl.find('.modal-body style, .modal-body link').appendTo($('head'));
 
-			paginationUrl = modalEl.find('#bitnoise-media-list').attr('data-pagination-url');
+            paginationUrl = modalEl.find('#bitnoise-media-list').attr('data-pagination-url');
 
-			modalEl.modal({
-					show 	: false,
-					keyboard: true,
-					backdrop: (!modalElWrap.hasClass('expanded'))
-				});
+            modalEl.modal({
+                    show    : false,
+                    keyboard: true,
+                    backdrop: (!modalElWrap.hasClass('expanded'))
+                });
 
-			modalEl.find('#bitnoise-media-list .item img').click(function(){
-				$('#bitnoise-media-list .item img').removeClass('selected');
-				$(this).addClass('selected');
-			});
+            modalEl.find('#bitnoise-media-list .item').click(function(){
+                $('#bitnoise-media-list .item').removeClass('selected');
+                $(this).addClass('selected');
+            });
 
-			modalEl.find('.submit').click(function(){
-				var selected = $('#bitnoise-media-list .item img.selected');
+            modalEl.find('.submit').click(function(){
+                var selected = $('#bitnoise-media-list .item.selected');
 
-				if(selected.length > 0) {
-					var id = selected.parent().attr('data-id');
+                if(selected.length > 0) {
+                    var id = selected.attr('data-id');
 
-					if(!isCke) {
-						if(typeof openedFrom.callback !== 'undefined') {
-							openedFrom.callback(id);
-						}
-						else {
-							callback(id);
-						}
-					}
-					else {
-						OpenFile($(selected).attr('data-original'));
-					}
-					openedFrom.delButton.show();
-					modalEl.modal('hide');
-				}
-			});
+                    if(!isCke) {
+                        if(typeof openedFrom.callback !== 'undefined') {
+                            openedFrom.callback(id);
+                        }
+                        else {
+                            callback(id);
+                        }
+                    }
+                    else {
+                        OpenFile($(selected).attr('data-original'));
+                    }
+                    openedFrom.delButton.show();
+                    modalEl.modal('hide');
+                }
+            });
 
-			$(document).on('hidden', '.modal', function () {
-			    $(this).parent().remove();
-			});
+            $(document).on('hidden', '.modal', function () {
+                $(this).parent().remove();
+            });
 
-			bindModalNavigation();
-		};
+            bindModalNavigation();
+        };
 
-		function GetUrlParam( paramName )
-		{
-			var oRegex = new RegExp( '[\?&]' + paramName + '=([^&]+)', 'i' ) ;
-			var oMatch = oRegex.exec( window.top.location.search ) ;
+        function GetUrlParam( paramName )
+        {
+            var oRegex = new RegExp( '[\?&]' + paramName + '=([^&]+)', 'i' ) ;
+            var oMatch = oRegex.exec( window.top.location.search ) ;
 
-			if (oMatch && oMatch.length > 1 )
-				return decodeURIComponent( oMatch[1] ) ;
-			else
-				return '' ;
-		}
+            if (oMatch && oMatch.length > 1 )
+                return decodeURIComponent( oMatch[1] ) ;
+            else
+                return '' ;
+        }
 
-		function OpenFile( fileUrl )
-		{
-			//PATCH: Using CKEditors API we set the file in preview window.
+        function OpenFile( fileUrl )
+        {
+            //PATCH: Using CKEditors API we set the file in preview window.
 
-			funcNum = GetUrlParam('CKEditorFuncNum') ;
-			//fixed the issue: images are not displayed in preview window when filename contain spaces due encodeURI encoding already encoded fileUrl
-			window.top.opener.CKEDITOR.tools.callFunction( funcNum, fileUrl);
+            funcNum = GetUrlParam('CKEditorFuncNum') ;
+            //fixed the issue: images are not displayed in preview window when filename contain spaces due encodeURI encoding already encoded fileUrl
+            window.top.opener.CKEDITOR.tools.callFunction( funcNum, fileUrl);
 
-			///////////////////////////////////
-			window.top.close() ;
-			window.top.opener.focus();
-		}
-
-
-		var getModal = function() {
-			$.get(modalUrl, function(response){
-				modalElWrap = $(response);
-
-				modalEl 	= modalElWrap.find('.modal');
-				modalEl.show();
-			});
-		};
+            ///////////////////////////////////
+            window.top.close() ;
+            window.top.opener.focus();
+        }
 
 
-		var openModal = function() {
-			bindModalBehaviors();
+        var getModal = function() {
+            $.get(modalUrl, function(response){
+                modalElWrap = $(response);
 
-			modalElWrap.appendTo($('body'));
-			modalEl.modal('show');
+                modalEl     = modalElWrap.find('.modal');
+                modalEl.show();
+            });
+        };
 
-			$('html, body').animate({
-		        scrollTop: modalEl.offset().top
-		    }, 400);
-		};
 
-		var bindMediaModal = function(el, callback) {
-			getModal();
+        var openModal = function() {
+            bindModalBehaviors();
 
-			el.click(openModal);
+            modalElWrap.appendTo($('body'));
+            modalEl.modal('show');
 
-			openedFrom 			= el;
-			openedFrom.callback = callback;
-		};
+            $('html, body').animate({
+                scrollTop: modalEl.offset().top
+            }, 400);
+        };
 
-		var setDeleteButton = function() {
-			var newEl = $(deleteButton);
+        var bindMediaModal = function(el, callback) {
+            getModal();
 
-			//hide if image wasn't choose
-			if (mediaSelects.val() == '') {
-				newEl.hide();
-			}
-			//bind reset hidden select and button text
-			newEl.click(function(){
-				mediaSelects.val(null);
-				if (openedFrom == null) {
-					resetButton(newEl.prev());
-				} else {
-					resetButton(openedFrom);
-				}
-				$(this).hide();
-	        });
+            el.click(openModal);
 
-	        return newEl;
-		};
+            openedFrom          = el;
+            openedFrom.callback = callback;
+        };
 
-		if(mediaSelects.length > 0) {
-			getModal();
-		}
+        var setDeleteButton = function() {
+            var newEl = $(deleteButton);
 
-		var isCke 		= false;
-		var searchParts = window.location.search.replace('?', '').split('&');
+            //hide if image wasn't choose
+            if (mediaSelects.val() == '') {
+                newEl.hide();
+            }
+            //bind reset hidden select and button text
+            newEl.click(function(){
+                mediaSelects.val(null);
+                if (openedFrom == null) {
+                    resetButton(newEl.prev());
+                } else {
+                    resetButton(openedFrom);
+                }
+                $(this).hide();
+            });
 
-		for(var i in searchParts) {
-			if(searchParts[i].split('=')[0] === 'CKEditor') {
-				isCke = true;
-				break;
-			}
-		}
+            return newEl;
+        };
 
-		if(isCke) {
-			// console.log($('div'));
-			modalElWrap = $('div').first();
-			modalEl 	= modalElWrap.find('.modal');
-			modalEl.show();
+        if(mediaSelects.length > 0) {
+            getModal();
+        }
 
-			openModal();
-		}
+        var isCke       = false;
+        var searchParts = window.location.search.replace('?', '').split('&');
 
-		window.btnMedia = {
-			bind : bindMediaModal
-		};
+        for(var i in searchParts) {
+            if(searchParts[i].split('=')[0] === 'CKEditor') {
+                isCke = true;
+                break;
+            }
+        }
 
-		$.each(mediaSelects, function(key, el){
-	        $(el).hide();
+        if(isCke) {
+            // console.log($('div'));
+            modalElWrap = $('div').first();
+            modalEl     = modalElWrap.find('.modal');
+            modalEl.show();
 
-	        var newEl = $(button);
-	        var delButton = setDeleteButton();
+            openModal();
+        }
 
-	        newEl.delButton = delButton;
+        window.btnMedia = {
+            bind : bindMediaModal
+        };
 
-			updateButton(newEl, el);
+        $.each(mediaSelects, function(key, el){
+            $(el).hide();
 
-	        newEl.click(function(){
-	        	openedFrom = newEl;
-	            openModal();
-	        });
+            var newEl = $(button);
+            var delButton = setDeleteButton();
 
-	        newEl.insertAfter(el);
-	        delButton.insertAfter(newEl);
-	    });
-	});
+            newEl.delButton = delButton;
+
+            updateButton(newEl, el);
+
+            newEl.click(function(){
+                openedFrom = newEl;
+                openModal();
+            });
+
+            newEl.insertAfter(el);
+            delButton.insertAfter(newEl);
+        });
+    });
 })(jQuery);
