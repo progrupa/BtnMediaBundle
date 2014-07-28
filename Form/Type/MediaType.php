b8bce529da8521442dee6ea4392420e83a45a651
Adam Misiorny <adam.misiorny@gmail.com>
2014-07-28 15:00:35 +0200

    add form type for media files

---
+++ Form/Type/MediaType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Btn\MediaBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class MediaType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'empty_value'   => 'btn_mediabundle.type.media.empty_value',
+            'label'         => 'btn_mediabundle.type.media.label',
+            'class'         => 'BtnMediaBundle:MediaFile',
+            'attr'          => array('class' => 'btn-media'),
+            'query_builder' => function (EntityRepository $em) {
+                return $em
+                    ->createQueryBuilder('mf')
+                    ->orderBy('mf.name', 'ASC');
+            },
+            'property' => 'name',
+            'required' => true,
+            'expanded' => false,
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'entity';
+    }
+
+    public function getName()
+    {
+        return 'btn_mediabundle_media';
+    }
+}
