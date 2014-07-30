ffaa2ee49b239fe6b9e33904792068d149104675
Adam Misiorny <adam.misiorny@gmail.com>
2014-07-30 09:34:42 +0200

    remove duplicate loading services

--- DependencyInjection/BtnMediaExtension.php
+++ DependencyInjection/BtnMediaExtension.php
@@ -22,15 +22,11 @@ class BtnMediaExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-
         $config['template'] = isset($config['template']) ? $config['template'] : 'BtnMediaBundle:Media:category.html.twig';
 
         $container->setParameter('btn_media', $config);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-
         $loader->load('services.yml');
     }
 }
