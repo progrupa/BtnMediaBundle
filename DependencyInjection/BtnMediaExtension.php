f404c94bea1665b02da14416f311d58735f2d2a8
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-11 18:17:30 +0200

    split service

--- DependencyInjection/BtnMediaExtension.php
+++ DependencyInjection/BtnMediaExtension.php
@@ -27,5 +27,7 @@ class BtnMediaExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('forms.yml');
+        $loader->load('menus.yml');
     }
 }
