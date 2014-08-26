7117cf9813ae0759ebe1232ee546c4f86e065e8f
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-26 17:20:49 +0200

    add compiler pass to set up file browser route

--- src/Btn/MediaBundle/BtnMediaBundle.php
+++ src/Btn/MediaBundle/BtnMediaBundle.php
@@ -3,7 +3,18 @@
 namespace Btn\MediaBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Btn\MediaBundle\DependencyInjection\Compiler;
 
 class BtnMediaBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new Compiler\WysiwygCompilerPass());
+    }
 }
---
+++ src/Btn/MediaBundle/DependencyInjection/Compiler/WysiwygCompilerPass.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Btn\MediaBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+class WysiwygCompilerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $wysiwygId = 'btn_admin.form.type.wysiwyg';
+
+        if (!$container->hasDefinition($wysiwygId)) {
+            return;
+        }
+
+        $wysiwyg = $container->getDefinition($wysiwygId);
+        $wysiwyg->addMethodCall('setFilebrowserBrowseRoute', array('btn_media_mediacontrol_modal'));
+    }
+}
